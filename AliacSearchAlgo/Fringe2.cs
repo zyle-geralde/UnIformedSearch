72229ea1a4b5bbfd622b2db851b84cd0369d7e45
zyle-geralde <zylgerdlp19@gmail.com>
2024-09-30 20:37:50 +0800

    initial commit

---
+++ AliacSearchAlgo/Fringe2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample
+{
+    class Fringe2:Fringes
+{
+        Queue<Node> s;
+        private HashSet<Node> visitedNodes;
+        public Fringe2() 
+        {
+            s = new Queue<Node>();
+            visitedNodes = new HashSet<Node>();
+        }
+        
+        public void add(Node n,Node origin)
+        {
+            if (visitedNodes.Contains(n))
+            {
+                n.Origin = n.Origin;
+            }
+            else
+            {
+                n.Origin = origin;
+                s.Enqueue(n);
+                visitedNodes.Add(n);
+            }
+        }
+
+        public Node remove()
+        {
+            if (s.Count != 0)
+                 return s.Dequeue();
+            return null;
+        }
+    }
+}
