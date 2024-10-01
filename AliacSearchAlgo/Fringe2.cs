5e8abc0182bba48ffba1286afc4cc130ade5dc4c
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-01 16:07:20 +0800

    minor error fixed

--- AliacSearchAlgo/Fringe2.cs
+++ AliacSearchAlgo/Fringe2.cs
@@ -9,25 +9,15 @@ namespace AISearchSample
     class Fringe2:Fringes
 {
         Queue<Node> s;
-        private HashSet<Node> visitedNodes;
         public Fringe2() 
         {
             s = new Queue<Node>();
-            visitedNodes = new HashSet<Node>();
         }
         
         public void add(Node n,Node origin)
         {
-            if (visitedNodes.Contains(n))
-            {
-                n.Origin = n.Origin;
-            }
-            else
-            {
-                n.Origin = origin;
-                s.Enqueue(n);
-                visitedNodes.Add(n);
-            }
+            n.Origin = origin;
+            s.Enqueue(n);
         }
 
         public Node remove()
